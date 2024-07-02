3ce5662a26b99cdd96923be9e7607e06a13259ee
sm <syd.morano@gmail.com>
2024-07-02 15:05:39 -0700

    Initial add

---
+++ src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,11 @@
+package com.pluralsight;
+
+import com.pluralsight.application.DeliApp;
+
+public class Main {
+    public static void main(String[] args) {
+        //System.out.println("Hello world!");
+        DeliApp app = new DeliApp();
+        app.run();
+    }
+}
