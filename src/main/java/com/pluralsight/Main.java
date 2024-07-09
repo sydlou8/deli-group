43b868f46573860c57f8702711075b26a9e31c7c
Volodymyr199606 <166452639+Volodymyr199606@users.noreply.github.com>
2024-07-09 15:40:20 -0700

    created a new @RestController class to handle HTTP requests, and updated main method to start a Spring Boot application

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,11 +1,14 @@
 package com.pluralsight;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.pluralsight.application.DeliApp;
 
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        //System.out.println("Hello world!");
         DeliApp app = new DeliApp();
         app.run();
+        SpringApplication.run(Main.class, args);
     }
 }
